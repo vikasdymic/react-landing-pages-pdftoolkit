58fb116919504827452d03af3f22970ef7f3f0ec
Dymic Digital <tech@dymic.com>
2019-07-19 12:56:08 -0700

    revised language

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -22,10 +22,10 @@ class Hero extends Component {
         <div className='heroContent'>
           <img className='heroImage' src={HeroImage} />
           <h1 className='heroContentTitle'>
-            Erase your file headaches with <span className='magicalLine'>PDFToolKit</span>.
+            Convert your files to PDF <span className='magicalLine'>for free</span>.
           </h1>
           <p className='heroContentCopy'>
-          This is an official Apple product and endorsed by Google! Normally $99, we are giving away PDFToolKit for free during our limited time offer. Having trouble opening files on your computer? Use PDFToolKit to open <i>ANY and EVERY</i> file you have on your computer. Keep up with your family members and don't miss out on any special moments. Open any file on your computer!
+          Click on the 'download' button to download PDFToolKit. Start converting files quickly and easily with PDFToolKit.
           </p>
           {/* <Button buttonStyle='large' buttonLabel='Download PDFToolKit' /> */}
           <button className='large button download' onClick={this.toggleModal}>Download PDFToolKit</button>
